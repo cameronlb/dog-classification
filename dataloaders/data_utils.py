129d182365a1612fdaf6cc42e258793f76bfe9d5
Cameron <cameronbateman94@gmail.com>
2022-07-03 11:24:45 +0100

    implemented basic dataframe for stanford and tsinghua datasets, with breed and file path cols.

--- dataloaders/data_utils.py
+++ dataloaders/data_utils.py
@@ -1,75 +1,47 @@
 import os
 import sys
+import re
 import pandas as pd
+import pathlib
 from PIL import Image
-def get_breeds_from_folder(data_dir):
-	""" Returns a list of all breeds """
 
-	path = os.path.join(sys.path[1], data_dir)
-	dir_list = os.listdir(path)
+def path_to_pd(img_dir_path):
 
-	stanford, tsinghua = None, None
-	if 'stanford' in data_dir: stanford = True
-	elif 'tsinghua' in data_dir: tsinghua = True
-	else: return print("Unrecognized path, exception raised")
+	full_path = pathlib.Path(img_dir_path)
+	print(full_path)
 
-	breeds = []
-	for folder_name in dir_list:
-		if stanford:
-			head_tail = folder_name.split('-', 1)
-			breeds.append(head_tail[1])
+	files = full_path.rglob('*.jpg')
 
-		elif tsinghua:
-			head_tail = folder_name.split('-', 2)
-			breeds.append(head_tail[2])
-
-	return breeds
-
-def get_image_paths(data_dir, breed):
-	# IMPROVE: remove second for loop, and use os.walk + file to return path
-	path = os.path.join(sys.path[1], data_dir)
-	image_folder_path = None
-	image_files = None
-
-	print("Searching directory: \"{}\"... with: \"{}\".\n".format(path.split("data", 1)[1],
-																						breed))
-	for (root, __, __) in os.walk(path, topdown=False):
-		if breed in root:
-			image_folder_path = root
-			print("Found Directory: \"{}\".\n".format(root))
-
-			for (__, __, files) in os.walk(image_folder_path, topdown=False):
-				image_files = files
+	folder_names = []
+	file_paths = []
+	file_names = []
+	for fname in files:
+		if fname.is_file():
+			# print(fname.parts)
+			file_paths.append(fname)
+			file_names.append(fname.parts[-1])
+			folder_names.append(fname.parts[-2])
 
 
-	# maybe change this to return full path using os.join()
-	image_paths = [os.path.join(image_folder_path, f_name) for f_name in image_files]
-	return image_paths
+	data_frame = {}
+	data_frame.update({"breed_name": folder_names})
+	data_frame.update({"file_path" : file_paths})
+	data_frame.update({"file_name" : file_names})
 
-def get_breed_from_file(file_path):
-	head, tail = file_path.split('\\', 1)
-	print("head: {}".format(head))
-	print("tail: {}".format(tail))
-	breed = None
-	return breed
+	df = pd.DataFrame(data_frame)
+	print(df)
+	print(df.head())
+
+	return df
 
 if __name__ == '__main__':
 	'''
 	data utility functions used for processing data with in the data directory for dataloaders.
 	'''
-	STANFORD_DATA_DIR = "data\\stanford_dataset\\images"
-	TSINGHUA_DATA_DIR = "data\\tsinghua_dataset\\low-resolution"
 
+	STANFORD_DATA_DIR_PATH = r"C:\Users\Cameron\Documents\python projects\dog " \
+							 "classification\data\stanford_dataset\images"
+	TSINGHUA_DATA_DIR_PATH = r"C:\Users\Cameron\Documents\python projects\dog " \
+							 r"classification\data\tsinghua_dataset\low-resolution"
 
-	tsinghua_breeds = get_breeds_from_folder(TSINGHUA_DATA_DIR)
-	stanford_breeds = get_breeds_from_folder(STANFORD_DATA_DIR)
-	stanford_breeds.sort()
-	tsinghua_breeds.sort()
-
-	# print("TSINGHUA #{} BREEDS: {} \n\n".format(len(tsinghua_breeds), tsinghua_breeds),
-	# 	  "STANFORD #{} BREEDS: {} \n".format(len(stanford_breeds), stanford_breeds))
-
-	images = get_image_paths(TSINGHUA_DATA_DIR, tsinghua_breeds[0])
-
-	get_breed_from_file(images[0])
-
+	path_to_pd(TSINGHUA_DATA_DIR_PATH)
