1228d3c338df1ce0c31dd6d81c677807d8c78d53
Cameron <cameronbateman94@gmail.com>
2022-07-04 19:37:38 +0100

    added parsed breed names to data frame.

--- dataloaders/data_utils.py
+++ dataloaders/data_utils.py
@@ -8,31 +8,39 @@ from PIL import Image
 def path_to_pd(img_dir_path):
 
 	full_path = pathlib.Path(img_dir_path)
-	print(full_path)
+	print("Converting data path into pandas data frame: \"{}\"".format(full_path))
+
+	breed_match = re.compile("([a-zA-Z][a-zA-Z].*[a-zA-Z])")
 
 	files = full_path.rglob('*.jpg')
 
+	breed_names = []
 	folder_names = []
 	file_paths = []
 	file_names = []
+	# fname is absolute file path
 	for fname in files:
 		if fname.is_file():
-			# print(fname.parts)
+
 			file_paths.append(fname)
 			file_names.append(fname.parts[-1])
 			folder_names.append(fname.parts[-2])
 
+			match = re.search(breed_match, fname.parts[-2])
+			if match:
+				breed_names.append(match.group())
 
-	data_frame = {}
-	data_frame.update({"breed_name": folder_names})
-	data_frame.update({"file_path" : file_paths})
-	data_frame.update({"file_name" : file_names})
+	data_frame = pd.DataFrame({'breed_name': breed_names,
+							   'folder_name': folder_names,
+							   'file_path' : file_paths,
+							   'file_name' : file_names})
 
-	df = pd.DataFrame(data_frame)
-	print(df)
-	print(df.head())
+	unique_breed_names = data_frame['breed_name'].unique()
+	print(unique_breed_names)
 
-	return df
+
+
+	return data_frame
 
 if __name__ == '__main__':
 	'''
@@ -44,4 +52,8 @@ if __name__ == '__main__':
 	TSINGHUA_DATA_DIR_PATH = r"C:\Users\Cameron\Documents\python projects\dog " \
 							 r"classification\data\tsinghua_dataset\low-resolution"
 
-	path_to_pd(TSINGHUA_DATA_DIR_PATH)
+	df_tsing = path_to_pd(TSINGHUA_DATA_DIR_PATH)
+	df_stan = path_to_pd(STANFORD_DATA_DIR_PATH)
+
+	print(df_tsing)
+	print(df_stan["breed_name"])
